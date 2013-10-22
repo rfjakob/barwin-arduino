d88a19e2ef344303682b1f3819d76f5e286a3eeb
Jakob Unterwurzacher <jakobunt@gmail.com>
2013-10-23 01:10:31 +0200

    Add example code for controlling a servo and reading the ads1231

--- config.h
+++ config.h
@@ -6,14 +6,15 @@
 #define CONFIG_H
 
 
-#define ADS1231_CLK_PIN  1
-#define ADS1231_DATA_PIN 2
+#define ADS1231_DATA_PIN 7
+#define ADS1231_CLK_PIN  6
+
 // ADC counts per milligram
 #define ADS1231_DIVISOR  1
 // Zero offset, milligrams
 #define ADS1231_OFFSET   0
 
 // PWM pin for servo number 1
-#define SERVO1_PIN      13
+#define SERVO1_PIN      11
 
 #endif
--- src/sketch.ino
+++ src/sketch.ino
@@ -1,4 +1,10 @@
+#include <Servo.h> 
+
 #include <ads1231.h>
+
+#include "../config.h"
+
+Servo servo;
 
 void setup()
 {
@@ -6,18 +12,45 @@ void setup()
 	Serial.println("READY");
 	
 	ads1231_init();
+	
+	servo.attach(SERVO1_PIN);
 }
 
 void loop()
 {
 	long mg;
-    
-    mg=ads1231_get_milligrams();
-    if(mg > ADS1231_ERR)
-    {
-		Serial.print("Timeout, code ");
-		Serial.println(mg);
+	int pos=1000;
+	int dir=1;
+
+/*
+	while(1)
+	{
+		mg=ads1231_get_milligrams();
+		if(mg > ADS1231_ERR)
+		{
+			Serial.print("Timeout, code ");
+			Serial.println(mg - ADS1231_ERR);
+		}
+		else
+			Serial.println(mg);
 	}
-	else
-		Serial.println(mg);
+*/
+
+
+	while(1)
+	{
+		Serial.println(pos);
+		servo.writeMicroseconds(pos);
+
+		pos+=dir;
+		
+		if(pos>=2500)
+			dir=-1;
+		
+		if(pos<=500)
+			dir=1;
+		
+		delay(1);
+	}
+
 }
