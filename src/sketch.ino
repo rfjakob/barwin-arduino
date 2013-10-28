83a4b0df3e9a37d826bd18e3220fe3483ed8db01
lumbric <lumbric@gmail.com>
2013-10-28 23:28:51 +0100

    convert all tabs to spaces

--- src/sketch.ino
+++ src/sketch.ino
@@ -8,12 +8,12 @@ Servo servo;
 
 void setup()
 {
-	Serial.begin(115200);
-	Serial.println("READY");
-	
-	ads1231_init();
-	
-	servo.attach(SERVO1_PIN);
+    Serial.begin(115200);
+    Serial.println("READY");
+
+    ads1231_init();
+
+    servo.attach(SERVO1_PIN);
 }
 
 
@@ -30,39 +30,39 @@ int turn_until(int max_pos, long max_weight, int delay_ms) {
 
 void loop()
 {
-	long mg;
-	int pos=1000;
-	int dir=1;
+    long mg;
+    int pos=1000;
+    int dir=1;
 
 /*
-	while(1)
-	{
-		mg=ads1231_get_milligrams();
-		if(mg > ADS1231_ERR)
-		{
-			Serial.print("Timeout, code ");
-			Serial.println(mg - ADS1231_ERR);
-		}
-		else
-			Serial.println(mg);
-	}
+    while(1)
+    {
+        mg=ads1231_get_milligrams();
+        if(mg > ADS1231_ERR)
+        {
+            Serial.print("Timeout, code ");
+            Serial.println(mg - ADS1231_ERR);
+        }
+        else
+            Serial.println(mg);
+    }
 */
 
 
-	while(1)
-	{
-		Serial.println(pos);
-		servo.writeMicroseconds(pos);
+    while(1)
+    {
+        Serial.println(pos);
+        servo.writeMicroseconds(pos);
 
-		pos+=dir;
-		
-		if(pos>=2500)
-			dir=-1;
-		
-		if(pos<=500)
-			dir=1;
-		
-		delay(1);
-	}
+        pos+=dir;
+
+        if(pos>=2500)
+            dir=-1;
+
+        if(pos<=500)
+            dir=1;
+
+        delay(1);
+    }
 
 }
