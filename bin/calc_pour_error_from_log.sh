46df4c5d7d1c2e5e3170c70920d060154127bec7
lumbric (antares) <lumbric@gmail.com>
2017-11-24 23:18:33 +0100

    Add something to log analyis script and 2dos
    No idea what this is. Last modified on 2nd July 11:46.

--- bin/calc_pour_error_from_log.sh
+++ bin/calc_pour_error_from_log.sh
@@ -1,1 +1,7 @@
 grep statis serial*|sed  's/^.*requested_amount: //'|sed 's/, measured_amount: /+/'|grep -o '[0-9+-]*'|bc
+
+
+grep Stats barwin-arduino/log/serialRMI_2014-05-10_MQ.log|sed  's/^.*requested_amount: //'|sed 's/, measured_amount: /-/'|cut -d',' -f1|grep -P '^[0-9]+-[0-9]+'|bc
+
+
+
